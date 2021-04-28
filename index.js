c21a41f1ae61e34624272d9542b65e70b6e65157
Carlos Garcia Castellanos <chars24@gmail.com>
2021-04-28 12:57:35 -0500

    Primer servidor con Node.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hola nueva vida")
+const http = require('http')
+
+let notes = [
+    {
+        id: 1,
+        "content": "Tengo que empezar a crear contenido",
+        "date": "2019-05-30T17:30:31.0982",
+        "important": true
+    },
+    {
+        id: 2,
+        "content": "Para salir de la situación en la cual me encuentro",
+        "date": "2019-05-30T18:39:34.0912",
+        "important": false
+    },
+    {
+        id: 3,
+        "content": "Lo hare por mi mujer y mi goldita bella",
+        "date": "2019-05-30T19:20:25.2982",
+        "important": true
+    },
+]
+
+const app = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(notes))
+})
+
+const PORT = 3005
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)

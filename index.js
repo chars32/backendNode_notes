3ae373a0340dff8f03f83aca5b1e8698e0c1e5a9
Carlos Garcia Castellanos <chars24@gmail.com>
2021-06-08 12:29:04 -0500

    nstalando, creando un servidor y rutas con Express

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
-const http = require('http')
+// const http = require('http')
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
 
 let notes = [
-    {
-        id: 1,
-        "content": "Tengo que empezar a crear contenido",
-        "date": "2019-05-30T17:30:31.0982",
-        "important": true
-    },
-    {
-        id: 2,
-        "content": "Para salir de la situación en la cual me encuentro",
-        "date": "2019-05-30T18:39:34.0912",
-        "important": false
-    },
-    {
-        id: 3,
-        "content": "Lo hare por mi mujer y mi goldita bella",
-        "date": "2019-05-30T19:20:25.2982",
-        "important": true
-    },
-]
+  {
+    id: 1,
+    content: "Tengo que empezar a crear contenido",
+    date: "2019-05-30T17:30:31.0982",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Para salir de la situación en la cual me encuentro",
+    date: "2019-05-30T18:39:34.0912",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "Lo hare por mi , mi mujer y mi goldita bella",
+    date: "2019-05-30T19:20:25.2982",
+    important: true,
+  },
+];
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
-})
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, { 'Content-Type': 'application/json' })
+//     response.end(JSON.stringify(notes))
+// })
 
-const PORT = 3005
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.get("/", (request, response) => {
+  response.send("<h1>Hola nuevo mundo</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  response.json(note);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
